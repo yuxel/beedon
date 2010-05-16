67a246376b8bf3a384beee24a7709246e50eece9
Osman Yuksel <yuxel@sonsuzdongu.com>
2010-05-17 00:28:16 +0300

    Comment fixed

--- Config/View.php
+++ Config/View.php
@@ -2,9 +2,8 @@
 /* vim: set expandtab sw=4 ts=4 sts=4: */
 
 /**
- * View and template engine configuration files
+ * View and template engine configuration
  */
-
 class Config_View{
     const ENGINE      = "Smarty";
     const COMPILE_DIR = "Template/_Compiled";
