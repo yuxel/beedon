0765a5a8b9792dfd13c782ab844b5f8d15876e78
Osman Yuksel <yuxel@sonsuzdongu.com>
2010-07-04 22:19:26 +0300

    Application Config file

---
+++ Config/Application.php
@@ -0,0 +1,13 @@
+<?php
+/* vim: set expandtab sw=4 ts=4 sts=4: */
+
+/**
+ * Application config parameters
+ */
+class Config_Application{
+    const URI_SEPARATOR = "q"; //uri's from ?q
+    const URI_SPLITTER  = "/"; //uri's splitted by /
+    const DEFAULT_CONTROLLER  = "index";
+    const DEFAULT_ACTION  = "index"; 
+    const ERROR_CONTROLLER  = "error";
+}
