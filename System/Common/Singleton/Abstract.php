6baa6d8565c60d76d2383777b5f3065e0bbb9b90
Osman Yuksel <yuxel@sonsuzdongu.com>
2010-07-05 20:23:10 +0300

    Singleton moved to Common

---
+++ System/Common/Singleton/Abstract.php
@@ -0,0 +1,21 @@
+<?php
+/* vim: set expandtab sw=4 ts=4 sts=4: */
+
+/**
+ * Singleton Abstract class
+ */
+abstract class System_Common_Singleton_Abstract{
+    private static $_instance;
+
+    abstract protected function __construct();
+
+    public static function getInstance() 
+    {
+        if (!isset(self::$_instance)) {
+            $thisClass = get_called_class();
+            self::$_instance = new $thisClass;
+        }
+
+        return self::$_instance;
+    }
+}
