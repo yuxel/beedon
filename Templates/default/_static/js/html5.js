e7a0ffc2fc3e1cc320c61c7255471ceff77d7928
Osman Yüksel <yuxel@sonsuzdongu.com>
2010-07-12 01:39:35 -0700

    Fixed typo

--- Templates/default/_static/js/html5.js
+++ Templates/default/_static/js/html5.js
@@ -1,9 +1,9 @@
 //http://ejohn.org/blog/html5-shiv/
 
 ( function(document) {
-    var elements = ["header","nav","figure","footer","aside","article"];
+    var elements = ["header","nav","figure","footer","aside","article","hgroup"];
 
-    for(i=0; i<elements.lenth; i++) {
+    for(i=0; i<elements.length; i++) {
         document.createElement(elements[i]);
     }
 })(document);
