18acaccfd73d3620b2f934186dac165224d8fcdb
Osman Yüksel <yuxel@sonsuzdongu.com>
2010-07-12 13:58:55 -0700

    added "section" for html5 tag list to ie6

--- Templates/default/_static/js/html5.js
+++ Templates/default/_static/js/html5.js
@@ -1,7 +1,7 @@
 //http://ejohn.org/blog/html5-shiv/
 
 ( function(document) {
-    var elements = ["header","nav","figure","footer","aside","article","hgroup"];
+    var elements = ["header","nav","figure","footer","aside","article","section"];
 
     for(i=0; i<elements.length; i++) {
         document.createElement(elements[i]);
