f44ec69fc84d1fbcf6f27de774a8814f1bc84c1f
Osman Yuksel <yuxel@sonsuzdongu.com>
2010-11-19 01:05:40 +0200

    DSN changed

--- Config/Model.php
+++ Config/Model.php
@@ -3,6 +3,6 @@
 class Config_Model{
     const ENGINE      = "Doctrine";
     const ENGINE_PATH = "Bee/3rdParty/Doctrine/Doctrine.php";
-    const DSN         = "mysql://root:12345678@localhost/beedon";
+    const DSN         = "mysql://root:12345678@localhost/blog_yuxel_net";
     const MODEL_PATH  = "Models/";
 }
